61e525c3bf2d207fcc2e21557df21cc988e6ff18
sebastian-sobik <sebasob749@student.polsl.pl>
2022-10-05 20:11:24 +0200

    flights-release v1.0

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
 import puppeteer from "puppeteer";
 import colors from "colors";
+import {dates} from "./flights-data.js"
 
-const dates = [
-    {
-        date_there : "2022-10-20",
-        date_back  : "2022-10-23",
-    },
-    {
-        date_there : "2022-10-13",
-        date_back  : "2022-10-16",
-    },
-    {
-        date_there : "2022-10-27",
-        date_back  : "2022-10-30"
-    },
-    {
-        date_there : "2022-10-27",
-        date_back  : "2022-11-01"
-    },
-    {
-        date_there : "2022-10-27",
-        date_back  : "2022-10-31"
-    },
-    {
-        date_there : "2022-11-10",
-        date_back  : "2022-11-13"
-    },
-    {
-        date_there : "2022-11-17",
-        date_back  : "2022-11-20"
-    },
-    {
-        date_there : "2022-11-24",
-        date_back  : "2022-11-27"
-    }
-]
 const options = ["--url", "--invalid", "--object"];
 
 const show = (x, Function=null) => {console.log(
